7ec81f5d5865dfed8be351fa739d96d48632e49c
Philipp Regling <philippregling@googlemail.com>
2018-09-13 21:35:01 +0200

    commit

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -5,13 +5,12 @@ import io.ktor.auth.Authentication
 import io.ktor.auth.UserIdPrincipal
 import io.ktor.auth.jwt.jwt
 import io.ktor.client.request.forms.formData
-import io.ktor.features.CORS
-import io.ktor.features.CallLogging
-import io.ktor.features.ContentNegotiation
-import io.ktor.features.DefaultHeaders
+import io.ktor.features.*
 import io.ktor.gson.gson
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
@@ -66,24 +65,27 @@ fun Application.module() {
         auth(authService)
     }
 
+    install(StatusPages) {
+        exception<InvalidCredentialsException> { exception ->
+            call.respond(HttpStatusCode.Unauthorized, mapOf("OK" to false, "error" to (exception.message ?: "")))
+        }
+        exception<AuthenticationException> { cause ->
+            call.respond(HttpStatusCode.Unauthorized)
+        }
+        exception<AuthorizationException> { cause ->
+            call.respond(HttpStatusCode.Forbidden)
+        }
+
+    }
+
+
     routing {
         get("/") {
             call.respondText("HELLO WORLD!", contentType = io.ktor.http.ContentType.Text.Plain)
         }
     }
 //
-//        install(StatusPages) {
-//            exception<InvalidCredentialsException> { exception ->
-//                call.respond(HttpStatusCode.Unauthorized, mapOf("OK" to false, "error" to (exception.message ?: "")))
-//            }
-//            exception<AuthenticationException> { cause ->
-//                call.respond(HttpStatusCode.Unauthorized)
-//            }
-//            exception<AuthorizationException> { cause ->
-//                call.respond(HttpStatusCode.Forbidden)
-//            }
-//
-//        }
+
 //
 //
 //        authenticate {
