636dc66936157c277682cdf78657987d87aa87dc
Philipp Regling <philippregling@googlemail.com>
2018-09-24 23:25:22 +0200

    added expose annotation

--- src/main/kotlin/model/Message.kt
+++ src/main/kotlin/model/Message.kt
@@ -1,5 +1,6 @@
 package main.kotlin.model
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-data class Message(@SerializedName("message") val message: String? = null)
+data class Message(@Expose @SerializedName("message") val message: String? = null)
